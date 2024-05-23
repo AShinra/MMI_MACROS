0d2f3b6a48e83c02738534f5371bdf26d24a6402
unknown <jonpuray@gmail.com>
2024-05-23 10:40:04 +0800

    segregated links

--- pages/1_BSP.py
+++ pages/1_BSP.py
@@ -44,7 +44,7 @@ if st.session_state['bsp_raw'] != None:
                 row[4].value='PRINT'
                 row[5].value='ONLINE'
             
-            if row[2].hyperlink != None:
+            if row[2].hyperlink != None and row[2].value != None:
                 if row[6].value=='Online News':
                     row[5].value=row[2].hyperlink.target
                     row[5].hyperlink=row[5].value
