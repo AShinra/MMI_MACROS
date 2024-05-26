c315667d46a21b7eb27d539fa70cd45186ea24e7
unknown <jonpuray@gmail.com>
2024-05-27 03:33:32 +0800

    ready for formatting

--- pages/2_TRIAL.py
+++ pages/2_TRIAL.py
@@ -176,15 +176,10 @@ if st.session_state['bsp_raw'] != None:
             df['TYPE'] = pd.Categorical(df['TYPE'], ['Broadsheet', 'Tabloid', 'Provincial', 'Magazine', 'Online News', 'Blogs'])
             df.sort_values('TYPE')
 
-        # df_merged = pd.concat([new_dfs[0], new_dfs[1], new_dfs[2]], sort=False)
-        # df_merged.to_excel(REPORT_FILE, index=False, startrow=8)
+        df_merged = pd.concat([new_dfs[0], new_dfs[1], new_dfs[2]], sort=False)
+        df_merged.to_excel(REPORT_FILE, index=False, startrow=8)
         
-        s_row = 8
-        new_dfs[0].to_excel(REPORT_FILE, index=False, startrow=s_row)
-        l, w = new_dfs[0].shape
-
-        new_dfs[1].to_excel(REPORT_FILE, index=False, startrow=s_row+l, mode="a")
-
+        
 
         # wb = openpyxl.load_workbook(REPORT_FILE)
         # ws  = wb.active
