7f088513d59e9a822648401d43ce594d098b1ad8
unknown <jonpuray@gmail.com>
2024-05-27 16:50:54 +0800

    added bsp template excel

--- pages/2_TRIAL.py
+++ pages/2_TRIAL.py
@@ -161,7 +161,7 @@ if st.session_state['bsp_raw'] != None:
                                     #     pass
 
                                     similarity_ratio = similar_title(main_title.lower(), sub_title.lower())
-                                    if similarity_ratio < 0.8:
+                                    if similarity_ratio < 0.75:
                                         continue
                                     else:
                                         _df.at[j, 'ONLINE LINK'] = sub_link
