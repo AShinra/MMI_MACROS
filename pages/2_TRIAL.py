d5c35c657f0fca9d98948bb59f109c655bd04a13
unknown <jonpuray@gmail.com>
2024-05-27 16:31:19 +0800

    added bsp template excel

--- pages/2_TRIAL.py
+++ pages/2_TRIAL.py
@@ -153,10 +153,12 @@ if st.session_state['bsp_raw'] != None:
                                 if sub_delete == 'FPR DELETION':
                                     continue
                                 else:
+                                    
                                     similarity_ratio = similar_title(main_title.lower(), sub_title.lower())
                                     if similarity_ratio < 0.8:
                                         continue
                                     else:
+                                        sub_title = sub_title.replace(sub_title, sub_source)
                                         _df.at[j, 'ONLINE LINK'] = sub_link
                                         _df.at[k, 'DELETE'] = 'FOR DELETION'
 
---
+++ test.py
@@ -0,0 +1,6 @@
+s1 = "Gross borrowings drop 31% in April BusinessWorld Online"
+
+
+s1 = s1.replace('BusinessWorld Online', '')
+
+print(s1)
