76a77fb00e5b80e3198ce134c717ea29fd8e8772
unknown <jonpuray@gmail.com>
2024-05-28 14:32:52 +0800

    segregated links

--- pages/2_TRIAL.py
+++ pages/2_TRIAL.py
@@ -43,7 +43,7 @@ def sheet_formating(df):
         ws.cell(row=s_row+1, column=5).value = 'PRINT'
         ws.cell(row=s_row, column=1).value = cats[c]
         for i in _df.index:
-            st.write(f"test - {_df.at[i, 'DATE']}")
+            st.write(f"test - {_df.at[i, 'TITLE']}")
             ws.cell(row=s_row+1+i, column=1).value = _df.at[i, 'DATE']
             ws.cell(row=s_row+1+i, column=2).value = _df.at[i, 'SOURCE']
             ws.cell(row=s_row+1+i, column=3).value = _df.at[i, 'TITLE']
