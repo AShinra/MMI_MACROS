43f68d6945957d3b41456add032c7869280a4f40
unknown <jonpuray@gmail.com>
2024-06-01 16:38:03 +0800

    segregation of script

--- pages/1_BSP_Alert.py
+++ pages/1_BSP_Alert.py
@@ -294,7 +294,7 @@ with st.container(border=True):
     st.header('Bangko Sentral ng Pilipinas')
 
 with st.expander('See Instructions'):
-    st.write('Testing')
+    st.write('')
 
 
 st.file_uploader('Input Raw File', key='bsp_raw')
