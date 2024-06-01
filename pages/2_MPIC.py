4f7208b65b85196b5fe54cbfe7873e7026cf7a55
unknown <jonpuray@gmail.com>
2024-06-01 16:23:50 +0800

    segregation of script

--- pages/2_MPIC.py
+++ pages/2_MPIC.py
@@ -1,6 +1,13 @@
+import streamlit as st
 from Tools import bg_image
 
 
 
 
 bg_image()
+
+with st.container(border=True):
+    
+    st.header('Metro Pacific Investments Corporation')
+
+st.file_uploader('Input Raw File', key='mpic_raw')
