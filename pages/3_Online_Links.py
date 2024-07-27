73d1d54065a8d3bded6d59a791ecca016e94cb09
unknown <jonpuray@gmail.com>
2024-07-27 12:22:35 +0800

    created manila bulletin scraper

--- pages/3_Online_Links.py
+++ pages/3_Online_Links.py
@@ -72,7 +72,8 @@ def mb_scraper2():
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    st.write(soup)
+    results = soup.find_all('div', class_="col-sm-6 col-md-8 col-lg-8 col-12")
+    st.write(results)
 
     return
 
