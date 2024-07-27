e9b1669b5627c26adf95acb11bcb2ee1ef4dfaf8
unknown <jonpuray@gmail.com>
2024-07-27 21:36:57 +0800

    segregated links

--- pages/3_Online_Links.py
+++ pages/3_Online_Links.py
@@ -20,7 +20,7 @@ def mb_scraper():
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options) 
-    driver.get("https://www.hostinger.ph/tutorials/blog-examples")
+    driver.get("https://mb.com.ph/sitemap/")
 
     st.code(driver.page_source)
 
