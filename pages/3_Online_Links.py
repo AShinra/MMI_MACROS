f0b006ab9cf49aa1fa61959a336fd689643d05ef
unknown <jonpuray@gmail.com>
2024-07-27 17:44:25 +0800

    segregated links

--- pages/3_Online_Links.py
+++ pages/3_Online_Links.py
@@ -1,7 +1,7 @@
 # import webdriver 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.core.os_manager import ChromeType
@@ -20,13 +20,13 @@ def mb_scraper():
     st.write(chrome_path)
 
     # run firefox as headless
-    options_browser = webdriver.ChromeOptions()
-    options_browser.add_argument("--headless")
+    options = Options()
+    options.add_argument("--headless")
     # driver = webdriver.Chrome(options=options_browser)
     driver = webdriver.Chrome(service=Service(
                 ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
             ),
-                                  options=options_browser)
+                                  options=options)
 
     # create webdriver object 
     # driver = webdriver.Firefox()
