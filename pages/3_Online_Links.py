7469b1fa5428e9e9316b56d67809039eeade5c8b
unknown <jonpuray@gmail.com>
2024-07-27 21:30:10 +0800

    segregated links

--- pages/3_Online_Links.py
+++ pages/3_Online_Links.py
@@ -1,10 +1,5 @@
 # import webdriver 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.core.os_manager import ChromeType
 from pathlib import Path
 import streamlit as st
 import time
@@ -20,58 +15,69 @@ def mb_scraper():
 
     # st.write(chrome_path)
 
-    # run firefox as headless
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
-    st.write(driver)
-    # create webdriver object 
-    # driver = webdriver.Firefox()
+    with st.echo():
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        from webdriver_manager.chrome import ChromeDriverManager
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.core.os_manager import ChromeType
 
-    # get google.co.in 
-    driver.get("https://mb.com.ph/sitemap")
+        @st.cache_resource
+        def get_driver():
+            return webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
 
-    print(driver)
+        # run firefox as headless
+        options = Options()
+        options.add_argument("--headless")
+        driver = get_driver()
+        
+        # create webdriver object 
+        # driver = webdriver.Firefox()
 
-    driver.quit()
-    exit()
+        # get google.co.in 
+        driver.get("https://mb.com.ph/sitemap")
 
-    # link_objects = driver.find_elements(By.CLASS_NAME, 'title-label')
-    link_objects = driver.find_elements(By.CSS_SELECTOR, 'category.title-label')
+        st.code(driver.page_source)
 
-    all_category_links = {}
-    for i in link_objects:
-        link = i.get_attribute('href')
-        cat = i.text
-        if link not in [
-            'https://mb.com.ph/contact-us',
-            'https://mb.com.ph/corporate-governance',
-            'https://mb.com.ph/privacy-policy',
-            'https://mb.com.ph/our-company',
-            'https://mb.com.ph/terms-and-conditions',
-            None]:
-            if cat != '':
-                all_category_links[cat] = link
-                st.write(link)
+        driver.quit()
+        exit()
+
+        # link_objects = driver.find_elements(By.CLASS_NAME, 'title-label')
+        link_objects = driver.find_elements(By.CSS_SELECTOR, 'category.title-label')
+
+        all_category_links = {}
+        for i in link_objects:
+            link = i.get_attribute('href')
+            cat = i.text
+            if link not in [
+                'https://mb.com.ph/contact-us',
+                'https://mb.com.ph/corporate-governance',
+                'https://mb.com.ph/privacy-policy',
+                'https://mb.com.ph/our-company',
+                'https://mb.com.ph/terms-and-conditions',
+                None]:
+                if cat != '':
+                    all_category_links[cat] = link
+                    st.write(link)
+                    
+        for cat_name, cat_link in all_category_links.items():
+            driver.get(cat_link)
+
+            try:
+                element1 = driver.find_element(By.XPATH, '//*[@class="col-sm-6 col-md-8 col-lg-8 col-12"]')
+                cat_links = element1.find_elements(By.XPATH, '//*[@class="mb-font-article-title mt-0 mb-1"]/a')
                 
-    for cat_name, cat_link in all_category_links.items():
-        driver.get(cat_link)
-
-        try:
-            element1 = driver.find_element(By.XPATH, '//*[@class="col-sm-6 col-md-8 col-lg-8 col-12"]')
-            cat_links = element1.find_elements(By.XPATH, '//*[@class="mb-font-article-title mt-0 mb-1"]/a')
+            except:
+                pass
             
-        except:
-            pass
-        
-        else:
-            st.success(cat_name)
-            for i in cat_links:
-                art_link = i.get_attribute('href')
-                st.write(art_link)
+            else:
+                st.success(cat_name)
+                for i in cat_links:
+                    art_link = i.get_attribute('href')
+                    st.write(art_link)
 
-        
-    driver.quit()
+            
+        driver.quit()
     return
 
 def mb_scraper2():
