5d353ba0db05efd4cd7cd48670435ea03c04f33f
unknown <jonpuray@gmail.com>
2024-08-14 11:55:55 +0800

    added online scraper[A

--- pages/scrapers/scraper_mt.py
+++ pages/scrapers/scraper_mt.py
@@ -20,7 +20,8 @@ def mt():
                 articles = article_list.select('.item-row.item-row-2.flex-row')
             
             for article in articles:
-                st.write(article.find(class_='article-title-h4'))
+                st.write(article.find(class_='article-title-h4').find('a').text)
+                st.write(article.find(class_='article-title-h4').find('a').get('href'))
 
 
 
