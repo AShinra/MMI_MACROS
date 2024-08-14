081f1dcf1410be02508a2fd9a206a7470b084834
unknown <jonpuray@gmail.com>
2024-08-14 12:26:07 +0800

    added online scraper[A

--- pages/3_Get_Links.py
+++ pages/3_Get_Links.py
@@ -9,6 +9,18 @@ from pages.scrapers.scraper_mst import mst
 from pages.scrapers.scraper_mt import mt
 
 
+def link_count(links_collected):
+
+    with col2:
+        st.header('')
+
+        with col2.container(border=True, height=300):
+            st.subheader('Links Collected', )
+            st.subheader(links_collected)
+
+    return
+
+
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -34,24 +46,12 @@ with col1:
 if pro:
     if st.session_state['pub_sel_radio'] == ':blue[Manila Standard]':
         links_collected = mst()
-
-        with col2:
+        link_count(links_collected)
             
-            st.header('')
-
-            with col2.container(border=True, height=300):
-                st.subheader('Links Collected', )
-                st.subheader(links_collected)
+            
     elif st.session_state['pub_sel_radio'] == 'Manila Times':
         links_collected = mt()
-
-        with col2:
-            
-            st.header('')
-
-            with col2.container(border=True, height=300):
-                st.subheader('Links Collected', )
-                st.subheader(links_collected)
+        link_count(links_collected)
                 
     else:
         with col2:
