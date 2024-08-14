cd241d85b6090e526b010697ade0405be97eb371
unknown <jonpuray@gmail.com>
2024-08-14 16:33:15 +0800

    added businessmirror scraper

--- pages/3_Get_Links.py
+++ pages/3_Get_Links.py
@@ -8,18 +8,7 @@ import re
 from pages.scrapers.scraper_mst import mst
 from pages.scrapers.scraper_mt import mt
 from pages.scrapers.scraper_bm import bm
-
-
-def link_count(links_collected):
-
-    with col2:
-        st.header('')
-
-        with col2.container(border=True, height=300):
-            st.subheader('Links Collected', )
-            st.subheader(links_collected)
-
-    return
+from pages.scrapers.scraper_ps import ps
 
 
 st.set_page_config(layout="wide")
@@ -27,14 +16,14 @@ col1, col2 = st.columns(2)
 
 with col1:
 
-    st.header('Link Scraper')
-    with col1.container(border=True, height=300):
+    with col1.container(border=True, height=420):
+        st.header('Link Scraper')
 
         pub_sel = st.radio(
             'Select Online Publication to scrape',
             ('Manila Bulletin',
             'Inquirer.net',
-            'Philstar',
+            ':blue[Philstar]',
             ':blue[Business Mirror]',
             'Business World',
             ':blue[Manila Times]',
@@ -42,26 +31,48 @@ with col1:
             'Malaya Business Insight',
             'Daily Tribune'), key='pub_sel_radio')
 
-    pro = st.button(label='Process', use_container_width=True)
+        pro = st.button(label='Process', use_container_width=True)
 
 if pro:
     if st.session_state['pub_sel_radio'] == ':blue[Manila Standard]':
         links_collected = mst()
-        link_count(links_collected)
-            
-            
+
+        with col2:
+
+            with col2.container(border=True, height=420):
+                st.header('Links Collected', )
+                st.subheader(links_collected)
+                    
     elif st.session_state['pub_sel_radio'] == ':blue[Manila Times]':
         links_collected = mt()
-        link_count(links_collected)
+
+        with col2:
+
+            with col2.container(border=True, height=420):
+                st.header('Links Collected', )
+                st.subheader(links_collected)
     
     elif st.session_state['pub_sel_radio'] == ':blue[Business Mirror]':
         links_collected = bm()
-        link_count(links_collected)
+
+        with col2:
+
+            with col2.container(border=True, height=420):
+                st.header('Links Collected', )
+                st.subheader(links_collected)
+    
+    elif st.session_state['pub_sel_radio'] == ':blue[Philstar]':
+        links_collected = ps()
+
+        with col2:
+
+            with col2.container(border=True, height=420):
+                st.header('Links Collected', )
+                st.subheader(links_collected)
                 
     else:
         with col2:
-            st.header('')
-            with col2.container(border=True, height=300):
+            with col2.container(border=True, height=420):
                 st.error('Development Phase')
 
 
