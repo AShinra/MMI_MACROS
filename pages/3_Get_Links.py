634661ba2ea4c8be524d7b9837ba27e2d2a73ec6
unknown <jonpuray@gmail.com>
2024-08-14 10:13:32 +0800

    added online scraper[A

--- pages/3_Get_Links.py
+++ pages/3_Get_Links.py
@@ -12,7 +12,7 @@ col1, col2 = st.columns(2)
 with col1:
 
     st.header('Link Scraper')
-    with col1.container(border=True, height=150):
+    with col1.container(border=True, height=300):
 
         pub_sel = st.radio(
             'Select Online Publication to scrape',
