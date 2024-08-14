268c51a6c8d94acd58e43b8644b4e7657f8972ea
unknown <jonpuray@gmail.com>
2024-08-14 12:04:39 +0800

    added online scraper[A

--- pages/scrapers/scraper_mt.py
+++ pages/scrapers/scraper_mt.py
@@ -24,7 +24,10 @@ def mt():
                 _url = article.find(class_='article-title-h4').find('a').get('href')
                 _date = article.find(class_='roboto-a').text
 
-                st.write(_date[0])
+                for i in _date:
+                    st.write(i)
+
+                # st.write(_date)
                 st.write(_title)
                 st.write(_url)
 
