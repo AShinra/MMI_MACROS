7aa070cb70f138830f430ab79d55512fccb72e70
unknown <jonpuray@gmail.com>
2024-08-16 09:07:55 +0800

    added date filter and inquirer scraper

--- pages/3_Get_Links.py
+++ pages/3_Get_Links.py
@@ -3,15 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import streamlit_shadcn_ui as ui
 
 # scrapers
 from pages.scrapers.scraper_mst import mst
 from pages.scrapers.scraper_mt import mt
 from pages.scrapers.scraper_bm import bm
 from pages.scrapers.scraper_ps import ps
+from pages.scrapers.scraper_inq import inq
 
 
 st.set_page_config(layout="wide")
+
+my_range = ui.date_picker(label='Select Date Range', mode='range', key='my_range', default_value=None)
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -22,7 +27,7 @@ with col1:
         pub_sel = st.radio(
             'Select Online Publication to scrape',
             ('Manila Bulletin',
-            'Inquirer.net',
+            ':blue[Inquirer.net]',
             ':blue[Philstar]',
             ':blue[Business Mirror]',
             'Business World',
@@ -63,6 +68,15 @@ if pro:
     
     elif st.session_state['pub_sel_radio'] == ':blue[Philstar]':
         links_collected = ps()
+
+        with col2:
+
+            with col2.container(border=True, height=420):
+                st.header('Links Collected', )
+                st.subheader(links_collected)
+    
+    elif st.session_state['pub_sel_radio'] == ':blue[Inquirer.net]':
+        links_collected = inq(my_range)
 
         with col2:
 
---
+++ pages/scrapers/scraper_inq.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import time
+import random
+from datetime import datetime, timedelta
+
+
+def inq(my_range):
+
+    # list of user-agents
+    userAgents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36	'
+    ]
+
+    now_0 = datetime.today()
+    now_0_str = now_0.strftime('%Y-%m-%d')
+
+    now_1 = now_0 - timedelta(days=1)
+    now_1_str = now_1.strftime('%Y-%m-%d')
+
+    now_2 = now_0 - timedelta(days=2)
+    now_2_str = now_2.strftime('%Y-%m-%d')
+
+    now_3 = now_0 - timedelta(days=3)
+    now_3_str = now_3.strftime('%Y-%m-%d')
+
+    now_4 = now_0 - timedelta(days=4)
+    now_4_str = now_4.strftime('%Y-%m-%d')
+
+    urls = {
+        now_0:f'https://www.inquirer.net/article-index/?d={now_0_str}',
+        now_1:f'https://www.inquirer.net/article-index/?d={now_1_str}',
+        now_2:f'https://www.inquirer.net/article-index/?d={now_2_str}',
+        now_3:f'https://www.inquirer.net/article-index/?d={now_3_str}',
+        now_4:f'https://www.inquirer.net/article-index/?d={now_4_str}'
+    }
+
+    _dates = []
+    _titles = []
+    _urls = []
+
+    st.write(my_range)
+
+    # convert string to dateobject
+    st_date = datetime.strptime(my_range[0], '%Y-%m-%d').date()
+    en_date = datetime.strptime(my_range[1], '%Y-%m-%d').date()
+    
+
+    for k, v in urls.items():
+        response = requests.get(v, headers={'User-Agent':random.choice(userAgents)})
+
+        if response.status_code == 200:
+            html_content = response.content
+
+            soup = BeautifulSoup(html_content, 'html.parser')
+
+            article_group = soup.find(id='index-wrap')
+            articles = article_group.find_all('li')
+            for article in articles:
+                _date = k.date()
+                if _date >= st_date and _date <= en_date:
+                    date_str = _date.strftime('%B %d, %Y')
+                    _title = article.find('a').text
+                    _url = article.find('a').get('href')
+
+                    if _url in _urls:
+                        continue
+                    else:
+                        _dates.append(date_str)
+                        _titles.append(_title)
+                        _urls.append(_url)
+            
+
+    df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})
+
+    st.dataframe(df, hide_index=True)
+
+    return df.shape[0]
