7ed2cf7d21d7d8e31f1e8bbbe66c8b6bae5aada7
unknown <jonpuray@gmail.com>
2024-08-16 15:37:30 +0800

    added malaya scraper

--- pages/scrapers/scraper_bw.py
+++ pages/scrapers/scraper_bw.py
@@ -29,7 +29,7 @@ def bw(my_range):
     _titles = []
     _urls = []
     
-    for i in range(1, 31):
+    for i in range(1, 21):
         url = f'https://www.bworldonline.com/page/{i}/?s'
         # response = requests.get(url)
         response = requests.get(url, headers={'User-Agent':random.choice(userAgents)})
