1744196bac75cc534df33e9f4489818513ad5098
unknown <jonpuray@gmail.com>
2024-08-16 15:57:03 +0800

    added malaya scraper

--- pages/scrapers/scraper_bw.py
+++ pages/scrapers/scraper_bw.py
@@ -59,9 +59,10 @@ def bw(my_range):
                         _titles.append(_title)
                         _urls.append(_url)
 
-            df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})
-            st.dataframe(df, hide_index=True)
-            return df.shape[0]
         
         else:
             st.write(response.status_code)
+
+    df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})
+    st.dataframe(df, hide_index=True)
+    return df.shape[0]
