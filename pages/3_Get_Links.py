61d434ffd093afe3a8f65bc63115b336b103a7d7
unknown <jonpuray@gmail.com>
2024-09-17 10:09:04 +0800

    updated url fetcher

--- pages/3_Get_Links.py
+++ pages/3_Get_Links.py
@@ -10,11 +10,11 @@ from Tools import bg_image
 from pages.scrapers.scraper_mst import mst
 from pages.scrapers.scraper_mt import mt
 from pages.scrapers.scraper_bm import bm
-from pages.scrapers.scraper_ps import ps
+# from pages.scrapers.scraper_ps import ps
 from pages.scrapers.scraper_inq import inq
 from pages.scrapers.scraper_mal import mal
 from pages.scrapers.scraper_bw import bw
-from pages.scrapers.scraper_mb import mb
+# from pages.scrapers.scraper_mb import mb
 from pages.scrapers.scraper_bil import bil
 
 st.set_page_config(layout="wide")
@@ -31,16 +31,13 @@ with col1:
 
         pub_sel = st.radio(
             'Select Online Publication to scrape',
-            ('Manila Bulletin',
-            ':blue[Inquirer.net]',
-            'Philstar',
-            ':blue[Business Mirror]',
-            ':blue[Business World]',
-            ':blue[Manila Times]',
-            ':blue[Manila Standard]',
-            ':blue[Malaya Business Insight]',
-            'Daily Tribune',
-            ':blue[Bilyonaryo]'), key='pub_sel_radio')
+            ('Inquirer.net',
+            'Business Mirror',
+            'Business World',
+            'Manila Times',
+            'Manila Standard',
+            'Malaya Business Insight',
+            'Bilyonaryo'), key='pub_sel_radio')
 
         pro = st.button(label='Process', use_container_width=True)
 
@@ -48,7 +45,7 @@ if pro:
     if my_range in ['', None, []]:
         st.error('Please indicate date range')
     else:
-        if st.session_state['pub_sel_radio'] == ':blue[Manila Standard]':
+        if st.session_state['pub_sel_radio'] == 'Manila Standard':
             links_collected = mst(my_range)
 
             with col2:
@@ -57,7 +54,7 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
                         
-        elif st.session_state['pub_sel_radio'] == ':blue[Manila Times]':
+        elif st.session_state['pub_sel_radio'] == 'Manila Times':
             links_collected = mt(my_range)
 
             with col2:
@@ -66,7 +63,7 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Business Mirror]':
+        elif st.session_state['pub_sel_radio'] == 'Business Mirror':
             links_collected = bm(my_range)
 
             with col2:
@@ -75,16 +72,16 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Philstar]':
-            links_collected = ps(my_range)
+        # elif st.session_state['pub_sel_radio'] == 'Philstar':
+        #     links_collected = ps(my_range)
 
-            with col2:
+        #     with col2:
 
-                with col2.container(border=True, height=450):
-                    st.header('Links Collected', )
-                    st.subheader(links_collected)
+        #         with col2.container(border=True, height=450):
+        #             st.header('Links Collected', )
+        #             st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Inquirer.net]':
+        elif st.session_state['pub_sel_radio'] == 'Inquirer.net':
             links_collected = inq(my_range)
 
             with col2:
@@ -93,7 +90,7 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Malaya Business Insight]':
+        elif st.session_state['pub_sel_radio'] == 'Malaya Business Insight':
             links_collected = mal(my_range)
 
             with col2:
@@ -102,7 +99,7 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Business World]':
+        elif st.session_state['pub_sel_radio'] == 'Business World':
             links_collected = bw(my_range)
 
             with col2:
@@ -111,16 +108,16 @@ if pro:
                     st.header('Links Collected', )
                     st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ' Manila Bulletin':
-            links_collected = mb(my_range)
+        # elif st.session_state['pub_sel_radio'] == 'Manila Bulletin':
+        #     links_collected = mb(my_range)
 
-            with col2:
+        #     with col2:
 
-                with col2.container(border=True, height=450):
-                    st.header('Links Collected', )
-                    st.subheader(links_collected)
+        #         with col2.container(border=True, height=450):
+        #             st.header('Links Collected', )
+        #             st.subheader(links_collected)
         
-        elif st.session_state['pub_sel_radio'] == ':blue[Bilyonaryo]':
+        elif st.session_state['pub_sel_radio'] == 'Bilyonaryo':
             links_collected = bil(my_range)
 
             with col2:
