f97e418835e493ff934d3342a5acbb43bf757989
unknown <jonpuray@gmail.com>
2024-09-19 09:51:25 +0800

    updated

--- pages/scrapers/fetcher_mb.py
+++ pages/scrapers/fetcher_mb.py
@@ -10,7 +10,7 @@ def mb():
 
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name('archive_key.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('fetcher_mb.json', scope)
 
     client = gspread.authorize(creds)
 
@@ -21,8 +21,4 @@ def mb():
     _article_dates = sheet.col_values(2)[:10]
     _urls = sheet.col_values(3)[:10]
 
-    df = pd.DataFrame({'Scraped Date':_scraped_dates, 'Article Date':_article_dates, 'URL':_urls})
-
-    return df
-
-
+    return pd.DataFrame({'Scraped Date':_scraped_dates, 'Article Date':_article_dates, 'URL':_urls})
