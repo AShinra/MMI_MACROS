52baa132cded91d471cedc47cd9bd02c8e149cc4
unknown <jonpuray@gmail.com>
2024-10-03 10:54:44 +0800

    removed tmt-newswire from manila times fetcher

--- pages/scrapers/scraper_mt.py
+++ pages/scrapers/scraper_mt.py
@@ -32,16 +32,17 @@ def mt(my_range):
             for article in articles:
                 _title = article.find(class_='article-title-h4').find('a').text
                 _url = article.find(class_='article-title-h4').find('a').get('href')
-                _date = article.find(class_='roboto-a').text
-                _date = re.sub('-\n', '', _date)
-                _date = re.sub('\n', '', _date)
-                _date = datetime.strptime(_date, '%B %d, %Y').date()
+                if 'tmt-newswire' not in _url:
+                    _date = article.find(class_='roboto-a').text
+                    _date = re.sub('-\n', '', _date)
+                    _date = re.sub('\n', '', _date)
+                    _date = datetime.strptime(_date, '%B %d, %Y').date()
 
-                if _date >= st_date and _date <= en_date:
-                    if _url not in _urls:
-                        _dates.append(_date)
-                        _titles.append(_title)
-                        _urls.append(_url)
+                    if _date >= st_date and _date <= en_date:
+                        if _url not in _urls:
+                            _dates.append(_date)
+                            _titles.append(_title)
+                            _urls.append(_url)
         
     df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})
 
