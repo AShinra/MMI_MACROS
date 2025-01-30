023720e3ccdd0023c597611974fee82dd60f11ae
Athan <jonpuray@gmail.com>
2025-01-30 11:21:03 +0800

    added delay on request

--- pages/scrapers/scraper_bil.py
+++ pages/scrapers/scraper_bil.py
@@ -35,6 +35,7 @@ def bil(my_range):
     # _day = my_range[1].split('-')[2]
     
         for i in range(1, 30):
+            response = 202
             while response == 202:
                 url = f'https://bilyonaryo.com/{_Y}/{_M}/{_D}/page/{i}/'
                 # url = f'https://bilyonaryo.com/{_year}/{_month}/{_day}/page/{i}/'
