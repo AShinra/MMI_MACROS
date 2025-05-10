41842b54f115c90c8519e6e32aa6738b51f94dcf
Athan <jonpuray@gmail.com>
2025-05-10 13:30:12 +0800

    added timeout to bilyonaryo

--- scrapers_pack/scraper_bil.py
+++ scrapers_pack/scraper_bil.py
@@ -35,7 +35,7 @@ def bil(my_range):
     # _day = my_range[1].split('-')[2]
     
         for i in range(1, 30):
-            with st.spinner('Processing Website', show_time=True):
+            with st.spinner('Processing Website'):
                 url = f'https://bilyonaryo.com/{_Y}/{_M}/{_D}/page/{i}/'
                 # url = f'https://bilyonaryo.com/{_year}/{_month}/{_day}/page/{i}/'
                 response = requests.get(url, headers={'User-Agent':random.choice(userAgents)})
