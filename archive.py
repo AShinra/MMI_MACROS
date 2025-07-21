0360e2d366c6e713e8584d2fd22f44543fd08c28
Athan <jonpuray@gmail.com>
2025-07-21 11:56:19 +0800

    updated

--- archive.py
+++ archive.py
@@ -29,7 +29,7 @@ def local_fetcher_archive():
             df = pd.DataFrame(value_list)
             df.columns = df.iloc[0]
             df = df[1:]
-            st.dataframe(df, use_container_width=True)
+            st.dataframe(df, use_container_width=True, hide_index=True)
 
         except Exception as e:
             st.error(f"Error accessing Google Sheet: {e}")
