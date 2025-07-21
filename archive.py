1ba0e77f98951c0a6c91ea3320068abd36e61cb6
Athan <jonpuray@gmail.com>
2025-07-21 11:41:44 +0800

    updated

--- archive.py
+++ archive.py
@@ -6,7 +6,13 @@ def local_fetcher_archive():
 
     pub_selection = st.radio(
         'Publication',
-        options=['Manila Bulletin', 'Philippine Star'])
+        options=['Manila Bulletin', 'Philippine Star'],
+        horizontal=True)
+    
+    if pub_selection == 'Manila Bulletin':
+        sheet_name = "Manila Bulletin"
+    elif pub_selection == 'Philippine Star':
+        sheet_name = "Philippine Star"
 
     client = get_gsheet_client()
 
@@ -14,7 +20,7 @@ def local_fetcher_archive():
         # sheet = client.open("Your Google Sheet Name").sheet1  # Update with your sheet name
         sheet_id = "1yU-_jdBAF4qYfdM9-dON0neMc77NF4VeDdeNpKlx79A"
         sheet = client.open_by_key(sheet_id)
-        value_list = sheet.worksheet('Manila Bulletin')
+        value_list = sheet.worksheet(sheet_name)
         # values_list = sheet.sheet1.row_values(1)
         st.write(value_list)
 
