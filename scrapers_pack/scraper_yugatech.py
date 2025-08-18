3af82723e2fe0ff052a7d980069dbd05720600f8
Athan <jonpuray@gmail.com>
2025-08-18 21:46:01 +0800

    added yugatech

--- scrapers_pack/scraper_yugatech.py
+++ scrapers_pack/scraper_yugatech.py
@@ -37,58 +37,52 @@ def yugatech(my_range, timer):
         _D = _date.day
 
         with st.spinner('Processing Website'):
-            for i in range(1, 31):
+            for i in range(1, 3):
                 if status=='break':
                     break
                 else:
-                    url = f'https://philstartech.com/all-posts/page/{i}/'
+                    url = f'https://www.yugatech.com/page/{i}/'
                     response = requests.get(url, headers={
                         'User-Agent':random.choice(userAgents),
                         'Referer': 'https://www.google.com/',  # Mimic a search engine referral
                         'Accept-Language': 'en-US,en;q=0.9'})
                     time.sleep(timer)
-                    
-                    # response = scraper.get(url, headers={
-                    #     'User-Agent':random.choice(userAgents),
-                    #     'Referer': 'https://www.google.com/',  # Mimic a search engine referral
-                    #     'Accept-Language': 'en-US,en;q=0.9'})
-
-                    "https://philstartech.com/news/2025/08/18/14124/dost-expands-ai-projects-in-ph/"
-
-                                
+                                                                        
                     if response.status_code == 200:
                         html_content = response.content
 
                         soup = BeautifulSoup(html_content, 'html.parser')
-                        container = soup.select('.loop-grid-base')
-                        for _container in container:
-                            elements = _container.find_all(class_='grid-base-post')
-                            for element in elements:
-                                link = element.find('a').get('href')
-                                _title = element.find('a').text
-                                if link not in [None, '']:
-                                    if re.search('/\d{4}/\d{2}/\d{2}/\w+', link):
-                                        if 'twitter.com/share?' not in link:
-                                            if 'facebook.com/share' not in link:
-                                                if 'viber://forward?text' not in link:
-                                                    if link not in _urls:
+                        container = soup.select_one('.bde-loop-list')
+                        elements = container.select('.bde-loop-item')
+                        for element in elements:
+                            link = element.find('a').get('href')
+                            print(link)
+                            _title = element.find('a').text
+                            print(_title)
 
-                                                        _day = int(link.split('/')[-4])
-                                                        _month = int(link.split('/')[-5])
-                                                        _year = int(link.split('/')[-6])
-                                                        _date = datetime(_year, _month, _day)
+                            # if link not in [None, '']:
+                            #     if re.search('/\d{4}/\d{2}/\d{2}/\w+', link):
+                            #         if 'twitter.com/share?' not in link:
+                            #             if 'facebook.com/share' not in link:
+                            #                 if 'viber://forward?text' not in link:
+                            #                     if link not in _urls:
 
-                                                        if _date in date_list:
-                                                            _urls.append(link)
-                                                            _dates.append(_date)
-                                                            _titles.append(_title)
-                                                        else:
-                                                            status='break'
+                            #                         _day = int(link.split('/')[-4])
+                            #                         _month = int(link.split('/')[-5])
+                            #                         _year = int(link.split('/')[-6])
+                            #                         _date = datetime(_year, _month, _day)
 
-                    else:
-                        st.write(response)
-                        break
+                            #                         if _date in date_list:
+                            #                             _urls.append(link)
+                            #                             _dates.append(_date)
+                            #                             _titles.append(_title)
+                            #                         else:
+                            #                             status='break'
+
+    #                 else:
+    #                     st.write(response)
+    #                     break
                                                 
-            df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})  
+    #         df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})  
 
-    return df
+    # return df
