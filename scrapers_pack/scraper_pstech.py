09e88f12166c4f1bd736a382488e64b06ff9d6e0
Athan <jonpuray@gmail.com>
2025-08-18 14:26:37 +0800

    added philstartech

--- scrapers_pack/scraper_pstech.py
+++ scrapers_pack/scraper_pstech.py
@@ -29,6 +29,8 @@ def ps_tech(my_range, timer):
     D = 'D'
     date_list = pd.date_range(st_date, en_date, freq=D)
 
+    status = ''
+
     for _date in date_list:
         _Y = _date.year
         _M = _date.month
@@ -36,54 +38,57 @@ def ps_tech(my_range, timer):
 
         with st.spinner('Processing Website'):
             for i in range(1, 31):
-                url = f'https://philstartech.com/all-posts/page/{i}/'
-                response = requests.get(url, headers={
-                    'User-Agent':random.choice(userAgents),
-                    'Referer': 'https://www.google.com/',  # Mimic a search engine referral
-                    'Accept-Language': 'en-US,en;q=0.9'})
-                time.sleep(timer)
-                
-                # response = scraper.get(url, headers={
-                #     'User-Agent':random.choice(userAgents),
-                #     'Referer': 'https://www.google.com/',  # Mimic a search engine referral
-                #     'Accept-Language': 'en-US,en;q=0.9'})
-
-                "https://philstartech.com/news/2025/08/18/14124/dost-expands-ai-projects-in-ph/"
-
-                            
-                if response.status_code == 200:
-                    html_content = response.content
-
-                    soup = BeautifulSoup(html_content, 'html.parser')
-                    container = soup.select('.loop-grid-base')
-                    for _container in container:
-                        elements = _container.find_all(class_='grid-base-post')
-                        for element in elements:
-                            link = element.find('a').get('href')
-                            _title = element.find('a').text
-                            if link not in [None, '']:
-                                if re.search('/\d{4}/\d{2}/\d{2}/\w+', link):
-                                    if 'twitter.com/share?' not in link:
-                                        if 'facebook.com/share' not in link:
-                                            if 'viber://forward?text' not in link:
-                                                if link not in _urls:
-
-                                                    _day = int(link.split('/')[-4])
-                                                    _month = int(link.split('/')[-5])
-                                                    _year = int(link.split('/')[-6])
-                                                    _date = datetime(_year, _month, _day)
-
-                                                    if _date in date_list:
-                                                        _urls.append(link)
-                                                        _dates.append(_date)
-                                                        _titles.append(_title)
-                                                    else:
-                                                        break
-
-                else:
-                    st.write(response)
+                if status=='break':
                     break
-                                            
-        df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})  
+                else:
+                    url = f'https://philstartech.com/all-posts/page/{i}/'
+                    response = requests.get(url, headers={
+                        'User-Agent':random.choice(userAgents),
+                        'Referer': 'https://www.google.com/',  # Mimic a search engine referral
+                        'Accept-Language': 'en-US,en;q=0.9'})
+                    time.sleep(timer)
+                    
+                    # response = scraper.get(url, headers={
+                    #     'User-Agent':random.choice(userAgents),
+                    #     'Referer': 'https://www.google.com/',  # Mimic a search engine referral
+                    #     'Accept-Language': 'en-US,en;q=0.9'})
+
+                    "https://philstartech.com/news/2025/08/18/14124/dost-expands-ai-projects-in-ph/"
+
+                                
+                    if response.status_code == 200:
+                        html_content = response.content
+
+                        soup = BeautifulSoup(html_content, 'html.parser')
+                        container = soup.select('.loop-grid-base')
+                        for _container in container:
+                            elements = _container.find_all(class_='grid-base-post')
+                            for element in elements:
+                                link = element.find('a').get('href')
+                                _title = element.find('a').text
+                                if link not in [None, '']:
+                                    if re.search('/\d{4}/\d{2}/\d{2}/\w+', link):
+                                        if 'twitter.com/share?' not in link:
+                                            if 'facebook.com/share' not in link:
+                                                if 'viber://forward?text' not in link:
+                                                    if link not in _urls:
+
+                                                        _day = int(link.split('/')[-4])
+                                                        _month = int(link.split('/')[-5])
+                                                        _year = int(link.split('/')[-6])
+                                                        _date = datetime(_year, _month, _day)
+
+                                                        if _date in date_list:
+                                                            _urls.append(link)
+                                                            _dates.append(_date)
+                                                            _titles.append(_title)
+                                                        else:
+                                                            status='break'
+
+                    else:
+                        st.write(response)
+                        break
+                                                
+            df = pd.DataFrame({'Date':_dates, 'Title':_titles, 'URL':_urls})  
 
     return df
