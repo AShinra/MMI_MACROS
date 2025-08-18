810c3939e5a81ddf811830e6c6b032e3482856e8
Athan <jonpuray@gmail.com>
2025-08-18 21:51:13 +0800

    added yugatech

--- scrapers_pack/scraper_yugatech.py
+++ scrapers_pack/scraper_yugatech.py
@@ -57,7 +57,7 @@ def yugatech(my_range, timer):
                         for element in elements:
                             link = element.find('a').get('href')
                             st.write(link)
-                            _title = element.find('a').text
+                            _title = element.find('h3').text
                             st.write(_title)
 
                             # if link not in [None, '']:
