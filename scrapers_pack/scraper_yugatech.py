5b10fd17ce69e47f36caf900d1e6a1c67ed5667f
Athan <jonpuray@gmail.com>
2025-08-18 22:16:09 +0800

    added yugatech

--- scrapers_pack/scraper_yugatech.py
+++ scrapers_pack/scraper_yugatech.py
@@ -61,7 +61,7 @@ def yugatech(my_range, timer):
                             # _ele = _element.find('span').text
                             # st.write(_ele)
 
-                            _date = element.select('span.ee-postmeta-date').text
+                            _date = element.select('.ee-postmeta-date').text
                             st.write(f'{_date} - {_title}')
 
                             # if link not in [None, '']:
